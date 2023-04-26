86c3401a1747cbc1e0e55b56601f16594caa4a33
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-04-26 14:56:28 -0300

    Atualização do programa + Savings Account

--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
 
+	private Double interestRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interestRate) {
+		super(number, holder, balance);
+		this.interestRate = interestRate;
+	}
+
+	public Double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(Double interestRate) {
+		this.interestRate = interestRate;
+	}
+	
+	public void updateBalance() {
+		balance += balance * interestRate;
+	}
+	
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
 }

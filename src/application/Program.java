44e3b6b2a3b06fc0c846a6951645af2ea3db108a
Maria Eduarda <mariaeduardasilv916@gmail.com>
2023-04-27 14:46:12 -0300

    Changes in the program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,38 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 public class Program {
 
 	public static void main(String[] args) {
 	
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		Locale.setDefault(Locale.US);
+		List<Account> list = new ArrayList<>();
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
-
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+		
+		double sum = 0.0;
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+			
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for(Account acc : list) {
+			acc.deposit(10.0);
+		}
+		for(Account acc : list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
 
 }
